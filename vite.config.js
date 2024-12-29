16f51df77d14d70a564c61ac841d8d40f0403ba3
Anahad06 <anahadd006@gmail.com>
2024-12-29 00:02:00 -0500

    fixed

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,33 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          [
+            'babel-plugin-styled-components',
+            {
+              displayName: true,
+              fileName: false
+            }
+          ]
+        ]
+      }
+    })
+  ],
+  resolve: {
+    alias: {
+      'styled-components': 'styled-components',
+    },
+  },
+  optimizeDeps: {
+    include: ['styled-components'],
+  },
   build: {
-    rollupOptions: {
-      external: ['styled-components']
-    }
-  }
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
 })
+
